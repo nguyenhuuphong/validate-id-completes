a9235511e2bc7cfb1971bd7617dc33761e3ac16c
Glitch (lizard-strengthend-mosquito) <none>
2020-12-04 08:14:54 +0000

    Import of nguyenhuuphong/validate-user-input ./README.md:25419550/1817

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-## h1 example
+# hello-express
 
-Thêm validation cho trang tạo user, check xem tên user có > 30 ký tự thì không cho phép nhập, quay lại trang tạo user và hiển thị lỗi
+A server that serves a webpage, its resources, and some data
+
+
+## Your Project
+
+On the front-end,
+
+- Edit `views/index.html` to change the content of the webpage
+- `public/client.js` is the javacript that runs when you load the webpage
+- `public/style.css` is the styles for `views/index.html`
+- Drag in `assets`, like images or music, to add them to your project
+
+On the back-end,
+
+- your app starts at `server.js`
+- add frameworks and packages in `package.json`
+- safely store app secrets in `.env` (nobody can see this but you and people you invite)
+
+Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
+
 
 ## Made by [Glitch](https://glitch.com/)
 
--- server.js
+++ server.js
@@ -1,34 +1,92 @@
 // server.js
 // where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const shortid = require("shortid");
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+var db = require('./db');
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.set('view engine', 'pug');
+app.set("views", "./views");
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+// database
+db.defaults({ books: [] }).write();
+//
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+var bookRoute = require('./routers/book.router');
+app.use('/books', bookRoute);
+
+var userRoute = require('./routers/users.router');
+app.use("/user", userRoute);
+
+var transRoute = require('./routers/trans.router');
+app.use('/transaction', transRoute)
+
+
+app.use(express.static('public')); 
+/*
+app.get("/books", (req, res) => {
+  res.render( "index", {
+    todo: db.get('books').value()
+});
+});  
+
+app.get("/books/create",(req, res) => {
+	res.render("create");
+});
+app.post("/books/create", (req, res) => {
+  req.body.id = shortid.generate();
+    db.get('books').push(req.body).write();
+  
+    res.redirect("/books");
+  
+});
+// xóa tên sách
+app.get("/books/:id/delete", function(req, res) {
+  var id = req.params.id;
+   var getData =  db
+    .get("books")
+    .remove({ id: id })
+    .write();
+  res.redirect("/books")
+ 
+  });
+
+
+// cập nhật tên sách
+app.get("/books/:id/update", function(req, res) {
+   var getId = req.params.id;
+  var getData = db
+    .get("books")
+    .find({ id: getId })
+    .value();
+  res.render("update", {
+    todo: getData
+  });
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.post("/books/:id/update", (req, res) => {
+  
+  var id = req.params.id;
+  var getTitle = req.body.title;
+  var getDes  = req.body.des;
+   
+   db.get('books')
+  .find({ id: id })
+  .assign({ title: getTitle, des: getDes})
+  .write()
+  
+  res.redirect("/books");
 });
+*/
